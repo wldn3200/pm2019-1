cd9a5a7b57694088ab8668aa1916e42e4799fbd5
jiwoo <wldn3200@naver.com>
2019-04-03 12:11:18 +0900

    2018-14491

--- practice.cpp
+++ practice.cpp
@@ -6,6 +6,6 @@ int main() {
 	/*
 	*  TODO: replace "XXXX-XXXXX" to your student number
 	*/
-	cout << "My student number is XXXX-XXXXX" << endl;
-	return 0
+	cout << "My student number is 2018-14491" << endl;
+	return 0;
 }
